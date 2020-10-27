0c217bfc7b2532052c4a9c8009c15ddca694e61b
Brandon <btiqui@gmail.com>
2020-10-26 21:21:10 -0700

    Separate pages

---
+++ products.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>JCS Music Studio</title>
+
+	<link rel="stylesheet" href="lib/css/styleguide.css">
+	<link rel="stylesheet" href="css/storetheme.css">
+</head>
+<body>
+
+  <?php include "php/components/navbar.php"; ?>
+
+  <div class="container">
+  	<article id="about" class="article card soft">
+  	  <div class="display-flex flex-align-center">
+  	  	<div class="flex-stretch">
+	  	  <h2>Products</h2>
+	  	</div>
+	  	<div class="flex-none">
+	  	  <small>5:20am</small>
+	  	</div>
+      </div>
+
+  	  <div class="article-body"></div>
+  	    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+  	    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+  	    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+  	    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+  	    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+  	    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  	    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+  	    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+  	    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+  	    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+  	    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+  	    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  	    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+  	    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+  	    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+  	    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+  	    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+  	    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  	</article>
+  </div>
+
+
+</body>
+</html>
