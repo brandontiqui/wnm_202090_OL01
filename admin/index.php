34e19e39f29e53244cba11512c2948110a25d76f
Brandon <btiqui@gmail.com>
2020-12-05 22:23:30 -0800

    Start product admin page

---
+++ admin/index.php
@@ -0,0 +1,25 @@
+<?php
+
+  include_once "../php/functions.php";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>KatsPlants&More - Edit User</title>
+
+	<base href="http://brandontiqui.com/WNM608/">
+
+	<?php include_once "../php/components/metadata.php"; ?>
+</head>
+<body>
+  <?php include "../php/components/admin_navbar.php"; ?>
+
+  <div class="container">
+  </div>
+
+
+</body>
+</html>
