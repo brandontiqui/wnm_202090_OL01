ba5c5cda763cb584b033f1d1eb0d6de499f6b343
Brandon <btiqui@gmail.com>
2020-12-05 23:18:09 -0800

    Show product list

--- admin/index.php
+++ admin/index.php
@@ -2,6 +2,35 @@
 
   include_once "../php/functions.php";
 
+  $empty_product = (object) [
+  	"product_id" => "",
+		"name" => "",
+		"category" => "",
+		"description" => "",
+		"price" => "",
+		"add_date" => "",
+		"modify_date" => "",
+		"thumbnail" => "",
+		"images" => "",
+		"number_in_stock" => "",
+		"amount" => "",
+		"total" => ""
+  ];
+
+  // TEMPLATES
+  function productListItem($r, $o) {
+		return $r.<<<HTML
+			<div><a href="{$_SERVER['PHP_SELF']}?id=$o->product_id">$o->name</a></div>
+		HTML;
+	}
+
+  function showProductPage($o) {
+		return $r.<<<HTML
+			<div>$o->name</div>
+			<div>$o->price</div>
+		HTML;
+	}
+
 ?>
 
 <!DOCTYPE html>
@@ -18,8 +47,25 @@
   <?php include "../php/components/admin_navbar.php"; ?>
 
   <div class="container">
+  	<?php
+  		if (isset($_GET['id'])) {
+  			echo showProductPage(
+  				$_GET['id'] == "new" ?
+  				  $empty_product :
+  				  makeQuery(makeConn(), "SELECT * FROM `products` WHERE `product_id`= ".$_GET['id'])[0]
+				);
+
+  			// showProductPage();
+  		} else {
+  	?>
+  	<h2>Product List</h2>
+		<?php
+
+				$result = makeQuery(makeConn(), "SELECT * FROM `products`");
+
+				echo array_reduce($result, 'productListItem');
+			}
+		?>
   </div>
-
-
 </body>
 </html>
--- php/components/admin_navbar.php
+++ php/components/admin_navbar.php
@@ -7,8 +7,8 @@
 	  <div class="flex-stretch"></div>
 	  <nav class="flex-none nav">
     <ul class="container display-flex">
-    	<li><a href="index.php">Product list</a></li>
-		  <li><a href="products.php">Add a new product</a></li>
+    	<li><a href="<?= $_SERVER['PHP_SELF'] ?>">Product list</a></li>
+		  <li><a href="<?= $_SERVER['PHP_SELF'] ?>">Add a new product</a></li>
     </ul>
 	  </nav>
 	</div>
